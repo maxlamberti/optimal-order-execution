edf04259524c80ed9b345470cace3e78c5713b6d
maxlamberti <maximilien.lamberti@gmail.com>
2020-01-29 17:51:14 -0800

    adds custom credential loading to s3 data download script

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Exploring optimal order execution policies on limit order books
 
 Execute the script ```scripts/scrapers/download_s3_data.py``` with command line arguments like:
 
-```python download_s3_data.py --pair=XBTZUSD --bucket=my-bucket --save_directory=/path/to/save/location```
+```python download_s3_data.py --pair=XBTZUSD --bucket=my-bucket --save_directory=/path/to/save/location --access_key_id=KEY_ID --secret_access_key=SECRET_KEY```
 
 Valid parameters for pair argument: {ALL, USDTZUSD, XETHZUSD, XXMRZUSD, XXRPZUSD, XREPZUSD, XBTZUSD}
--- scripts/scrapers/download_s3_data.py
+++ scripts/scrapers/download_s3_data.py
@@ -8,10 +8,21 @@ from tqdm import tqdm
 logging.basicConfig(format='[%(levelname)s] | %(asctime)s | %(message)s', level=logging.INFO)
 
 
-def download_folder_from_s3(bucket, bucket_folder, save_directory=None):
+def download_folder_from_s3(bucket, bucket_folder, save_directory=None, access_key_id=None, secret_access_key=None):
 	"""Download data to from S3 bucket."""
 
-	s3_resource = boto3.resource('s3')
+	# define aws resource
+	if (access_key_id is not None) and (secret_access_key is not None):
+		logging.info("Initializing AWS session with custom credentials.")
+		session = boto3.Session(
+			aws_access_key_id=args.access_key_id,
+			aws_secret_access_key=args.secret_access_key
+		)
+		s3_resource = session.resource('s3')
+	else:
+		s3_resource = boto3.resource('s3')
+
+	# access bucket
 	bucket = s3_resource.Bucket(bucket)
 	logging.info("Starting download...")
 	for object in tqdm(bucket.objects.filter(Prefix=bucket_folder)):
@@ -50,16 +61,32 @@ if __name__ == '__main__':
 		required=False,
 		help='Which currency pair to download data for. Set to ALL to get everything.'
 	)
+	parser.add_argument(
+		'-i',
+		'--access_key_id',
+		type=str,
+		default=None,
+		required=False,
+		help='Access key ID of AWS credentials.'
+	)
+	parser.add_argument(
+		'-k',
+		'--secret_access_key',
+		type=str,
+		default=None,
+		required=False,
+		help='Secret access key of AWS credentials.'
+	)
 	args = parser.parse_args()
 
 	# choose currencies to retrieve
 	if args.pair.upper() == 'ALL':
-		currencies = ['USDTZUSD', 'XETHZUSD', 'XXMRZUSD', 'XXRPZUSD', 'XREPZUSD', 'XBTZUSD']
+		currencies = ['USDTZUSD', 'XETHZUSD', 'XXMRZUSD', 'XXRPZUSD', 'XREPZUSD', 'XXBTZUSD']
 	else:
 		currencies = [args.pair]
 
 	for pair in currencies:
 		logging.info("Downloading data for pair {} from S3 {} bucket.".format(pair, args.bucket))
-		download_folder_from_s3(args.bucket, pair, args.save_directory)
+		download_folder_from_s3(args.bucket, pair, args.save_directory, args.access_key_id, args.secret_access_key)
 
 	logging.info("Finished downloading all data.")
