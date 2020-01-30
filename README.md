c8c02ff4cdb7953ccedc0af1aa10ac6ef23956d0
maxlamberti <maximilien.lamberti@gmail.com>
2020-01-29 18:54:24 -0800

    adds utils file with a helper method to load the data from multiple parquet files within a local directory

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Execute the script ```scripts/scrapers/download_s3_data.py``` with command line 
 ```python download_s3_data.py --pair=XBTZUSD --bucket=my-bucket --save_directory=/path/to/save/location --access_key_id=KEY_ID --secret_access_key=SECRET_KEY```
 
 Valid parameters for pair argument: {ALL, USDTZUSD, XETHZUSD, XXMRZUSD, XXRPZUSD, XREPZUSD, XBTZUSD}
+
+
+### Loading The Data Into Python
+
+Check ```scripts/utils/data_loading.py``` for the ```load_data(...)``` method. You can specify the directory containing the parquet data and the method will combine the separate files into a single data frame. In case of the limit order book data, the whole data set might not fit into memory. In that case, datetime parameters can be passed to only select the subset of data scraped within those dates. Exact instructions are in the docstring.
---
+++ scripts/utils/data_loading.py
@@ -0,0 +1,68 @@
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from datetime import datetime
+
+
+def load_data(data_directory, start_time=None, end_time=None):
+	"""Load parquet data from specified directory. By default loads all data within directory. However,
+	if too much data, user can apply date filter to only load data within dates.
+
+
+	PARAMETERS
+	----------
+	data_directory : str
+		Path which specifies the location contained the (compressed) parquet files.
+	start_time : datetime.datetime
+		(Optional) Loads all files created after this date. Can also pass timestamp if preferred.
+	end_time : datetime.datetime
+		(Optional, required with start_time) Loads files created before this date. Can also pass timestamp if preferred.
+
+	RETURNS
+	-------
+	pandas.DataFrame : A data frame containing the data in the parquet files.
+	"""
+
+	# select files within date inputs
+	files = np.array([f for f in os.listdir(data_directory) if os.path.isfile(os.path.join(data_directory, f))])
+
+	# if desired, only select files within date range
+	if (start_time is not None) and (end_time is not None):
+
+		# reformat time inputs in case they are timestamps
+		if not isinstance(start_time, datetime):
+			start_time = datetime.fromtimestamp(int(start_time))
+		if not isinstance(end_time, datetime):
+			end_time = datetime.fromtimestamp(int(end_time))
+
+		# apply date mask and select files
+		dates = np.array([datetime.fromtimestamp(int(f.split('.')[0])) for f in files])
+		file_mask = (start_time <= dates) & (dates < end_time)
+		files = files[file_mask]
+
+	# load selected data frames
+	data_frame_list = []
+	for file in tqdm(files):
+		filepath = os.path.join(data_directory, file)
+		df = pd.read_parquet(filepath)
+		data_frame_list.append(df)
+
+	return pd.concat(data_frame_list)
+
+
+if __name__ == '__main__':
+
+	# example usage
+
+	# parameters
+	path_to_data = '/Users/maxlamberti/Desktop/XETHZUSD'
+	start_timestamp = 1580109897
+	end_timestamp = 1580115303
+	start_time = datetime.fromtimestamp(start_timestamp)
+	end_time = datetime.fromtimestamp(end_timestamp)
+
+	# load data
+	df = load_data(path_to_data, start_time, end_time)
+	equivalent_df_using_timestamps = load_data(path_to_data, start_timestamp, end_timestamp)
+	# might_eat_your_ram_df = load_data(path_to_data)
